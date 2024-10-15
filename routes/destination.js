2766e85d02702ad9cf44ecd8c9d5f019a4b44d51
Bladedkiwi <bladedkiwi@byui.edu>
2024-10-15 12:26:35 -0500

    Base Destination Req Endpoints/Functions created

--- routes/destination.js
+++ routes/destination.js
@@ -1,8 +1,21 @@
 const {fishForErrors} = require("../handlers/errorHandlers");
+const destinationController = require("../controllers/destinationController");
 const router = require('express').Router();
 
-router.get('/', async (req, res) => {
-    res.send('Destination Space')
-})
+//TODO: Change GetDestinations by Id to maybe {username} instead? Maybe that's how we use the data from logging a person in with gitHub - Haven't looked into that
+
+/**
+ * Base Destination Routes
+ */
+router.get('/', fishForErrors(destinationController.getDestinations));
+router.get('/:id', fishForErrors(destinationController.getDestinationById));
+
+/**
+ * Protected Routes
+ */
+router.post('/', fishForErrors(destinationController.createDestination));
+router.put('/:id', fishForErrors(destinationController.updateDestination));
+router.delete('/:id', fishForErrors(destinationController.deleteDestination));
+
 
 module.exports = router;
